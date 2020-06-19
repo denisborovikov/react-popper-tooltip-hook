8b6bdbb16a6656f17ae54bf385aad7e091218409
Denis Borovikov <metaller.ru@gmail.com>
2020-06-18 21:42:33 -0600

    Add portal example

---
+++ src/Portal.js
@@ -0,0 +1,47 @@
+import React from "react";
+import "./styles.css";
+import { usePopperTooltip } from "./usePopperTooltip";
+import { createPortal } from "react-dom";
+
+const modifiers = [{ name: "offset", options: { offset: [10, 10] } }];
+
+export function PortalExample() {
+  const {
+    getArrowProps,
+    getTooltipProps,
+    setArrowRef,
+    setTooltipRef,
+    setTriggerRef,
+    visible,
+  } = usePopperTooltip(
+    {
+      trigger: "click",
+    },
+    {
+      modifiers,
+    }
+  );
+
+  return (
+    <>
+      <button type="button" ref={setTriggerRef}>
+        Reference element
+      </button>
+
+      {visible &&
+        createPortal(
+          <div
+            ref={setTooltipRef}
+            {...getTooltipProps({ className: "tooltip-container" })}
+          >
+            Popper element
+            <div
+              ref={setArrowRef}
+              {...getArrowProps({ className: "tooltip-arrow" })}
+            />
+          </div>,
+          document.body
+        )}
+    </>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import { BasicExample } from "./Basic";
+import { PortalExample } from "./Portal";
 import { FollowCursorExample } from "./FollowCursor";
 import { MutationObserverExample } from "./MutationObserver";
 
@@ -10,6 +11,8 @@ ReactDOM.render(
   <React.StrictMode>
     <h2>Basic example</h2>
     <BasicExample />
+    <h2>React portal example</h2>
+    <PortalExample />
     <h2>Follow cursor example</h2>
     <FollowCursorExample />
     <h2>Mutation observer</h2>
