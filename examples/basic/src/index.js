e47c9fbe75d6e32c1e70a87b6db79b1747fe643a
Denis Borovikov <metaller.ru@gmail.com>
2020-07-30 16:05:41 -0600

    Update examples

--- examples/basic/src/index.js
+++ examples/basic/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./styles.css";
-import { usePopperTooltip } from "react-popper-tooltip-hook/src/usePopperTooltip";
+import { usePopperTooltip } from "react-popper-tooltip-hook";
 
 const modifiers = [{ name: "offset", options: { offset: [0, 10] } }];
 
